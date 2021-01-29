f527c086bacae22cb5176f81e192af1e913c5c9c
Alex Tang <alextang58@yahoo.com>
2021-01-29 04:16:09 -0500

    Updated with react-masonry-css

--- src/ShowPhotos.js
+++ src/ShowPhotos.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import './App.css';
+import Masonry from 'react-masonry-css';
+
+const breakpointColumnsObj = {
+    default: 3,
+    1100: 2,
+    800: 1
+};
 
 function ShowPhotos(props){
-    function photosHelper(){
-        return props.photos.map( (apic, idx) => {
-            return <img className='pics' src={apic.photos[0].original_size.url} alt='from tumblr' />;
-        }
 
-        )
-    };
+
+    const pictas = props.photos.map( (apic,idx) => {
+        return (
+            <div className='picsdiv'>
+                <img className='pics' key={idx} src={apic.photos[0].original_size.url} alt='from tumblr' />
+            </div>
+        );
+    }
+    );
+
+    // function photosHelper(){
+    //     console.log(pictas);
+    // };
 
     return(
-        <section>{photosHelper()}</section>
+        <>
+            {/* <section>{photosHelper()}</section> */}
+            <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+            >
+                {pictas}
+            </Masonry>
+        </>
     );
 }
 
