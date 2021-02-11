86b152bcc427fa6e1c8987630f2946fd540722ba
Alex Tang <alextang58@yahoo.com>
2021-02-10 19:21:14 -0500

    better layout and scaling

--- src/Nav.js
+++ src/Nav.js
@@ -7,24 +7,27 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 function Nav(){
     return (
         <div className="overnav">
-            <Link style={{textDecoration: 'none', color: 'black'}} to="/">
-                <h3 className="header">PHOTOS BY ALEX TANG ©</h3>
-            </Link>
+            <div className="headerwrap">
+                <Link style={{textDecoration: 'none', color: 'black'}} to="/">
+                    <h3 className="header">Photos by Alex Tang ©</h3>
+                </Link>
+            </div>
             <input type="checkbox" id="toggle"></input>
             <nav className="navBar">
                 <ul>
-                    <li><a href="https://www.instagram.com/alexhtang/">IG: @ALEXHTANG</a></li>
+                    <li><a href="https://www.instagram.com/alexhtang/">IG: @alexhtang</a></li>
                     <li><a href="https://www.bostonglobe.com/lifestyle/2016/09/22/chronicling-his-city-and-his-adventures/yqagLtfu50aIvEC598DSrJ/story.html">GLOBE INTERVIEW</a></li>
-                    <li>FAVORITE PHOTOS</li>
-                    <li><a href="https://open.spotify.com/user/alxtng">MY SPOTIFY</a></li>
+                    {/* <li>FAVORITE PHOTOS</li> */}
+                    <li><a href="https://open.spotify.com/user/alxtng">My Spotify</a></li>
                     <Link style={{textDecoration: 'none', color: 'black'}} to="/about">
-                        <li>ABOUT</li> 
+                        <li>About</li> 
                     </Link>
                 </ul>
             </nav>
             <label for="toggle">
                 <FontAwesomeIcon className='baricon' icon={faBars} />
             </label>
+            <div className="credit"><p>Website created by Alex Tang</p></div>
         </div>
     )
 }
